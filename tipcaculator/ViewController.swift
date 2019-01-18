39696bedc4562c691533e77ec9dd0f491f36ec50
Jiade LIn <jdforest460@gmail.com>
2019-01-18 15:09:01 -0500

    Initial Commit

---
+++ tipcaculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipcaculator
+//
+//  Created by Jiade Lin on 1/18/19.
+//  Copyright © 2019 Jiade Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
